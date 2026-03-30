0c42efa9031f6e1e227adc0ff5d7e13c38bf76ed
Anaska47 <nskadiri@gmail.com>
2026-03-30 05:00:10 +0200

    chore: Pinterest verification tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
   verification: {
     google: "wnL49Tuf54VQIr6HH6XVw5pLTpq9O8rs8da7eEf401g",
     other: {
-      "p:domain_verify": "118ca06797a8f693394f2588a1429a15"
+      "p:domain_verify": "04a7aaaa5353af3fdc4b3899f0dd3b03"
     }
   },
 };
